7c135ae511332154ae3e5bfb479525f91d837d8d
Julio Figueiredo <julioo.1amorim@gmail.com>
2025-08-19 20:08:27 -0300

    feat: add db connection

---
+++ hello/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_PRODUCT (name, price) VALUES ('Notebook', 3000);
+INSERT INTO TBL_PRODUCT (name, price) VALUES ('Mouse Gamer', 250);
+INSERT INTO TBL_PRODUCT (name, price) VALUES ('Teclado Gamer', 300);
